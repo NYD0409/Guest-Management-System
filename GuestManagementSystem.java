9c338be43433a8400723b00dcef372e6e231cab6
NohYeongDeok <62379057+NYD0409@users.noreply.github.com>
2020-03-23 17:54:41 +0900

    Create GuestManagementSystem.java

---
+++ GuestManagementSystem.java
@@ -0,0 +1,14 @@
+package guestmanagementsystem;
+
+public class GuestManagementSystem {
+
+	public static void main(String[] args) {
+			System.out.println("Guest Management System Menu");
+			System.out.println(" 1. Add Guest");
+			System.out.println(" 2. Delete Guest");
+			System.out.println(" 3. Edit Guest");
+			System.out.println(" 4. View Guest");
+			System.out.println(" 5. Exit");
+			System.out.println("Select one number between 1 - 5:");
+	}
+}
