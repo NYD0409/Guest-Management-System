59c1e75c97af88bb07397ddfb7479bacda11ca72
ksgi0 <ksgi0@DESKTOP-AES47FU>
2020-03-30 19:15:59 +0900

    add menu selection

--- src/GuestManagementSystem.java
+++ src/GuestManagementSystem.java
@@ -1,12 +1,31 @@
+import java.util.*;
+
 public class GuestManagementSystem {
 
 	public static void main(String[] args) {
-			System.out.println("Guest Management System Menu");
-			System.out.println(" 1. Add Guest");
-			System.out.println(" 2. Delete Guest");
-			System.out.println(" 3. Edit Guest");
-			System.out.println(" 4. View Guest");
-			System.out.println(" 5. Exit");
-			System.out.println("Select one number between 1 - 5:");
+			Scanner input = new Scanner(System.in);
+			int num = 5;
+		
+			while (num != 6) {
+				System.out.println("Guest Management System Menu");
+				System.out.println(" 1. Add Guest");
+				System.out.println(" 2. Delete Guest");
+				System.out.println(" 3. Edit Guest");
+				System.out.println(" 4. View Guest");
+				System.out.println(" 5. Show a Menu");
+				System.out.println(" 6. Exit");
+				System.out.println("Select one number between 1 - 6:");
+				num = input.nextInt();
+				if (num == 1) {
+					addGuest();
+			}
+		}
+	}
+	
+	public static void addGuest() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Guest code");
+		int guestcode = input.nextInt();
+		System.out.println(guestcode);
 	}
 }
